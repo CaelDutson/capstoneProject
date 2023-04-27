0338b4401865ef40b5d97cd9f0e0d263c1a85d64
CaelDutson <cael.dutson7714@stu.mtec.edu>
2023-04-27 08:17:16 -0600

    massive admin update

--- client/src/AP.js
+++ client/src/AP.js
@@ -7,7 +7,10 @@ const AdminSignedIn = () => {
     const [users, setUsers] = useState(null);
     const [input, setInput] = useState({ 
         userName: ""
-    })  
+    }); 
+    const [currentUserId, setCurrentUserId] = useState(null);
+    const [userInputs, setUserInputs] = useState({});
+
     const [editInput, setEditInput] = useState({ 
         firstName: "", 
         lastName: "", 
@@ -17,7 +20,17 @@ const AdminSignedIn = () => {
         telephone: "", 
         email: "",
         id: null,
-    })  
+    });  
+    const [currentEditInput, setCurrentEditInput] = useState({ 
+        firstName: "", 
+        lastName: "", 
+        userName: "", 
+        password: "", 
+        address: "", 
+        telephone: "", 
+        email: "",
+        id: null,
+    }) 
     const handleInput = (e) => {
         setInput({...input, [e.target.name]: e.target.value})
     }  
@@ -26,10 +39,9 @@ const AdminSignedIn = () => {
         //console.log(editInput);
     //}   
     const handleInput2 = (e) => {
-        const { name, value } = e.target;  
-        setEditInput(prevState => ({...prevState, [name]: value}));
-        console.log(value); 
-        console.log(name);
+        const { name, value } = e.target;
+        setUserInputs(prevState => ({...prevState, [currentUserId]: {...prevState[currentUserId], [name]: value}})); 
+        console.log(editInput);
       };
     const search = async (e) => {
         e.preventDefault()
@@ -45,21 +57,23 @@ const AdminSignedIn = () => {
           }).then((res) => {console.log(res.data); setUserList(res.data)})
     };  
     const editUsers = async (e) => {
-        e.preventDefault();   
-        const { name, value } = e.target;  
-        setEditInput(prevState => ({...prevState, [name]: value}));
-        console.log(value); 
-        console.log(name);
-        await Axios({ 
-            method: "POST",
-            headers: {
-              Authorization: 'bearer ' + sessionStorage.getItem("data") || "invalid"
-            },
-            withCredentials: true, 
-            url: '/editUsers', 
-            data: editInput
-          }).then((res) => {console.log(res.data); setEditInput(res.data)})
-    }; 
+        e.preventDefault();
+        const updatedUserData = userInputs[currentUserId];
+        const userToUpdate = users.find(user => user.id === currentUserId);
+        const updatedUser = {...userToUpdate, ...updatedUserData};
+        await Axios({
+          method: "POST",
+          headers: {
+            Authorization: 'bearer ' + sessionStorage.getItem("data") || "invalid"
+          },
+          withCredentials: true,
+          url: '/editUsers',
+          data: updatedUser
+        }).then((res) => {
+          console.log(res.data);
+          setUserInputs(prevState => ({...prevState, [currentUserId]: res.data}));
+        });
+      };
     const deleteUser = async (e) => { 
         setEditInput({...editInput, [e.target.name]: e.target.value});
         e.preventDefault()
@@ -84,13 +98,23 @@ const AdminSignedIn = () => {
           url: '/getUsers', 
         }).then((res) => {
           console.log(res.data);
-          setUsers(res.data);
-        });
+          setUsers(res.data); 
+        }); 
       }  
 
       const handleButtonClick = (e) => {
-        Collapsible();
-        handleInput2(e);
+        Collapsible(); 
+        const parent = e.target.parentElement;
+        const inputs = parent.querySelectorAll('input'); 
+        const inputValues = {};
+        inputs.forEach((input) => {
+            inputValues[input.name] = input.value; 
+        
+        });
+        const userId = e.target.value;
+        setCurrentUserId(userId);
+        setUserInputs(prevState => ({...prevState, [userId]: {...prevState[userId], id: userId}}));
+        //handleInput2(e);
       };
 
       const Collapsible = () => { 
@@ -118,32 +142,23 @@ const AdminSignedIn = () => {
                         {userList ? <h1>Choose who to edit <ul>{userList.map((item)=><><li><input onChange={(e) => handleInput2(e)} type="radio" id={item.id} name="id" value={item.id} /><label for="html">{item.firstname} {item.lastname}  ID:{item.id}</label></li></>)}</ul></h1> : null}
                     </form>
                 </div> 
-            </div> 
-            <div className="editUsers">  
-                <div className="text"><h3>*Note: You must search and select a person first. If you leave any blank inputs, the changes will still be applied</h3></div> 
-                <div className="inputUsers">
-                    <form onSubmit={editUsers}>
-                        <input placeholder="First Name" onChange={(e) => handleInput2(e)} type='text' name="firstName"></input> 
-                        <input placeholder="Last Name" onChange={(e) => handleInput2(e)} type='text' name="lastName"></input> 
-                        <input placeholder="Username" onChange={(e) => handleInput2(e)} type='text' name="userName"></input> 
-                        <input placeholder="Password" onChange={(e) => handleInput2(e)} type='text' name="password"></input> 
-                        <input placeholder="Address" onChange={(e) => handleInput2(e)} type='text' name="address"></input> 
-                        <input placeholder="telephone" onChange={(e) => handleInput2(e)} type='text' name="telephone"></input> 
-                        <input placeholder="email" onChange={(e) => handleInput2(e)} type='text' name="email"></input> 
-                        <button>Submit</button> 
-                    </form>  
-                </div>
-                <form onSubmit={deleteUser}> 
-                    <button>DELETE</button>
-                </form>
-            </div> 
+            </div>  
             <div className="getUsers"> 
                 <div>
                     <h3>Display all students</h3>
                     <button onClick={getUsers}>Submit</button> 
                     {users ? <ul>{users.map((item)=><li key={item.id}><button className="collapsible" name="id" value={item.id} onClick={(e)=> {handleButtonClick(e)}}>{item.firstname} {item.lastname}</button>  
-                        <div id="collapsibleContent" className="userList">Username: <input id="username" onChange={(e) => handleInput2(e)} type='text' name="userName" value={editInput.userName !== '' ? editInput.userName : item.username}></input> ID:<input value={item.id}></input> Password: <input id="password" onChange={(e) => handleInput2(e)} type='text' name="password" value={editInput.password !== '' ? editInput.password : item.password}></input>Firstname: <input id="firstname" onChange={(e) => handleInput2(e)} type='text' name="firstName" value={editInput.firstName !== '' ? editInput.firstName : item.firstname}></input>Lastname: <input id="lastname" onChange={(e) => handleInput2(e)} type='text' name="lastName" value={editInput.lastName !== '' ? editInput.lastName : item.lastname}></input>Email: <input id="email" onChange={(e) => handleInput2(e)} type='text' name="email" value={editInput.email !== '' ? editInput.email : item.email}></input>Phone Number: <input id="telephone" onChange={(e) => handleInput2(e)} type='text' name="telephone" value={editInput.telephone !== '' ? editInput.telephone : item.telephone}></input>Address: <input id="address" onChange={(e) => handleInput2(e)} type='text' name="address" value={editInput.address !== '' ? editInput.address : item.address}></input><div><button onClick={(e) => editUsers(e)} id={item.id}>Save</button><button id={item.id}>Delete</button></div></div>  
-                        </li>)}</ul> : null}
+                        <div id="collapsibleContent" className="userList"> 
+                        Username: <input id="username" onChange={(e) => handleInput2(e)} type='text' name="userName" value={userInputs[item.id]?.userName || item.username}></input>  
+                        ID:<input value={item.id}></input>  
+                        Password: <input id="password" onChange={(e) => handleInput2(e)} type='text' name="password" value={userInputs[item.id]?.password || item.password}></input> 
+                        Firstname: <input id="firstname" onChange={(e) => handleInput2(e)} type='text' name="firstName" value={userInputs[item.id]?.firstName || item.firstname}></input> 
+                        Lastname: <input id="lastname" onChange={(e) => handleInput2(e)} type='text' name="lastName" value={userInputs[item.id]?.lastName || item.lastname}></input> 
+                        Email: <input id="email" onChange={(e) => handleInput2(e)} type='text' name="email" value={userInputs[item.id]?.email || item.email}></input> 
+                        Phone Number: <input id="telephone" onChange={(e) => handleInput2(e)} type='text' name="telephone" value={userInputs[item.id]?.telephone || item.telephone}></input> 
+                        Address: <input id="address" onChange={(e) => handleInput2(e)} type='text' name="address" value={userInputs[item.id]?.address || item.address}></input> 
+                        <div><button onClick={(e) => editUsers(e)} id={item.id}>Save</button><button id={item.id}>Delete</button></div></div>  
+                    </li>)}</ul> : null}
                 </div>
             </div> 
         </div>
