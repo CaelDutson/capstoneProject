7424b7f805442560a6050bc9af1c0cbc9379706c
CaelDutson <cael.dutson7714@stu.mtec.edu>
2023-05-01 20:01:32 -0600

    search admin changes

--- client/src/AP.js
+++ client/src/AP.js
@@ -4,6 +4,8 @@ import Axios from "axios";
 
 const AdminSignedIn = () => {  
     const [userList, setUserList] = useState(null);  
+    const [message, setMessage] = useState('...Loading'); 
+    const [message2, setMessage2] = useState(null);
     const [users, setUsers] = useState(null);
     const [input, setInput] = useState({ 
         userName: ""
@@ -43,7 +45,8 @@ const AdminSignedIn = () => {
         setUserInputs(prevState => ({...prevState, [currentUserId]: {...prevState[currentUserId], [name]: value}})); 
         console.log(editInput);
       };
-    const search = async (e) => {
+    const search = async (e) => { 
+        setMessage2('...Loading')
         e.preventDefault()
 
         await Axios({ 
@@ -54,7 +57,16 @@ const AdminSignedIn = () => {
             withCredentials: true, 
             url: '/getInfo', 
             data: input
-          }).then((res) => {console.log(res.data); setUserList(res.data)})
+          }).then((res) => {console.log(res.data);  
+
+            setUserList(res.data); 
+            const error = res.data; 
+            console.log(error.length);
+            if (error.length == 0){  
+                setUserList(false); 
+                setMessage2('Search Not Found');
+            }
+        })
     };  
     const editUsers = async (e) => {
         e.preventDefault();
@@ -148,7 +160,18 @@ const AdminSignedIn = () => {
                     <form onSubmit={(e) => search(e)}> 
                         <input onChange={(e) => handleInput(e)} name="userName"></input> 
                         <button value="submit">Submit</button> 
-                        {userList ? <h1>Choose who to edit <ul>{userList.map((item)=><><li><input onChange={(e) => handleInput2(e)} type="radio" id={item.id} name="id" value={item.id} /><label for="html">{item.firstname} {item.lastname}  ID:{item.id}</label></li></>)}</ul></h1> : null}
+                        {userList ? <ul className="editUsersList">{userList.map((item)=><li key={item.id}><button className="collapsible" name="id" value={item.id} onClick={(e)=> {handleButtonClick(e)}}>{item.firstname} {item.lastname}</button>  
+                            <div id="collapsibleContent" className="userList"> 
+                            Username: <input id="username" onChange={(e) => handleInput2(e)} type='text' name="userName" value={userInputs[item.id]?.userName || item.username}></input>  
+                            ID:<input value={item.id}></input>  
+                            Password: <input id="password" onChange={(e) => handleInput2(e)} type='text' name="password" value={userInputs[item.id]?.password || item.password}></input> 
+                            Firstname: <input id="firstname" onChange={(e) => handleInput2(e)} type='text' name="firstName" value={userInputs[item.id]?.firstName || item.firstname}></input> 
+                            Lastname: <input id="lastname" onChange={(e) => handleInput2(e)} type='text' name="lastName" value={userInputs[item.id]?.lastName || item.lastname}></input> 
+                            Email: <input id="email" onChange={(e) => handleInput2(e)} type='text' name="email" value={userInputs[item.id]?.email || item.email}></input> 
+                            Phone Number: <input id="telephone" onChange={(e) => handleInput2(e)} type='text' name="telephone" value={userInputs[item.id]?.telephone || item.telephone}></input> 
+                            Address: <input id="address" onChange={(e) => handleInput2(e)} type='text' name="address" value={userInputs[item.id]?.address || item.address}></input> 
+                            <div><button onClick={(e) => editUsers(e)} id={item.id}>Save</button><button onClick={(e) => deleteUser(e)} name="id" id={item.id}>Delete</button></div></div>  
+                        </li>)}</ul> : message2} 
                     </form>
                 </div> 
             </div>  
@@ -166,7 +189,7 @@ const AdminSignedIn = () => {
                             Phone Number: <input id="telephone" onChange={(e) => handleInput2(e)} type='text' name="telephone" value={userInputs[item.id]?.telephone || item.telephone}></input> 
                             Address: <input id="address" onChange={(e) => handleInput2(e)} type='text' name="address" value={userInputs[item.id]?.address || item.address}></input> 
                             <div><button onClick={(e) => editUsers(e)} id={item.id}>Save</button><button onClick={(e) => deleteUser(e)} name="id" id={item.id}>Delete</button></div></div>  
-                        </li>)}</ul> : null} 
+                        </li>)}</ul> : message} 
                     </div>
                 </div>
             </div> 
